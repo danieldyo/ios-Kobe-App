6a10c0f3c1536186842f3e5fd94f8d48e1d8ef13
Home <Home@homes-imac.local>
2015-12-24 22:29:48 -0800

    Initial Commit

---
+++ Kobe/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Kobe
+//
+//  Created by Home on 12/24/15.
+//  Copyright © 2015 DanielOng. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
